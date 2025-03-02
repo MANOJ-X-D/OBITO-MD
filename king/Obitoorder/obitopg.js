cd40a355ac7d67ab14393ca2ba5f8004a948fe39
MANOJ-X-D <hackerboyy728@gmail.com>
2025-03-02 20:10:22 +0530

    Add files via upload

---
+++ king/Obitoorder/obitopg.js
@@ -0,0 +1,48 @@
+import config from '../../config.cjs'; // Ensure this matches your project setup
+
+const ping = async (m, sock) => {
+  const prefix = config.PREFIX;
+  const cmd = m.body.startsWith(prefix)
+    ? m.body.slice(prefix.length).split(' ')[0].toLowerCase()
+    : '';
+  const text = m.body.slice(prefix.length + cmd.length).trim();
+
+  if (cmd === "ping") {
+    const start = new Date().getTime();
+    await m.React('⏳'); // React with a loading icon
+    const end = new Date().getTime();
+    const responseTime = (end - start).toFixed(2);
+
+    // Updated text style with OBITO-MD branding and response rate
+    const responseText = `*_OBITO-MD Speed is_* ${responseTime} ms`;
+
+    await m.React('✅'); // React with a success icon
+
+    sock.sendMessage(
+      m.from,
+      {
+        text: responseText,
+        contextInfo: {
+          isForwarded: false,
+          forwardedNewsletterMessageInfo: {
+            newsletterJid: '@newsletter',
+            newsletterName: "OBITO-MD",
+            serverMessageId: -1,
+          },
+          forwardingScore: 999, // Score to indicate it has been forwarded
+          externalAdReply: {
+            title: "✨ OBITO-MD ✨",
+            body: "Ping Speed Calculation",
+            thumbnailUrl: '', // Add thumbnail URL if required
+            sourceUrl: 'https://whatsapp.com/channel/0029Vb8MXtb3GJOqeYi8VF0p', // Add source URL if necessary
+            mediaType: 1,
+            renderLargerThumbnail: false,
+          },
+        },
+      },
+      { quoted: m }
+    );
+  }
+};
+
+export default ping;
